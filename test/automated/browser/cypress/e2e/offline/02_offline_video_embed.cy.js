d41babab6be2525117dc5d6ea95000ed7b67f1de
Richard Abrich <richard.abrich@gmail.com>
2023-08-01 09:24:36 -0400

    QAI commit for issue 1690896108

--- test/automated/browser/cypress/e2e/offline/02_offline_video_embed.cy.js
+++ test/automated/browser/cypress/e2e/offline/02_offline_video_embed.cy.js
@@ -1,15 +1,35 @@
-import { setup } from '../../support/setup.js';
-setup();
+// ***********************************************************
+// This example support/e2e.js is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
 
-describe(`Offline video embed`, () => {
-	it('Can visit the page', () => {
-		cy.visit('http://localhost:8080/embed/video');
-	});
+// Import commands.js using ES2015 syntax:
+import './commands';
+import fetchData from './fetchData.js';
 
-	// Offline banner
-	it('Has correct offline banner values', () => {
-		cy.contains('This stream is offline. Check back soon!').should(
-			'be.visible'
-		);
-	});
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
+
+// Put Owncast in a state where it's ready to be tested.
+
+// Set server URL
+fetchData('http://localhost:8080/api/admin/config/serverurl', {
+	method: 'POST',
+	data: { value: 'https://testing.biz' },
+});
+
+// Enable Fediverse features.
+fetchData('http://localhost:8080/api/admin/config/federation/enable', {
+	method: 'POST',
+	data: { value: true },
 });
