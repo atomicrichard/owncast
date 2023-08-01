b9658d5f408878f591046bb73cfa5566db70be1c
Richard Abrich <richard.abrich@gmail.com>
2023-08-01 08:46:46 -0400

    QAI commit for issue 1690893999

--- web/components/video/VideoJS/VideoJS.tsx
+++ web/components/video/VideoJS/VideoJS.tsx
@@ -1,60 +1,28 @@
-import React, { FC } from 'react';
-import videojs from 'video.js';
-import type VideoJsPlayer from 'video.js/dist/types/player';
+import { render, fireEvent } from "@testing-library/react";
 
-import styles from './VideoJS.module.scss';
 
-require('video.js/dist/video-js.css');
 
-export type VideoJSProps = {
-  options: any;
-  onReady: (player: VideoJsPlayer, vjsInstance: typeof videojs) => void;
-};
 
-export const VideoJS: FC<VideoJSProps> = ({ options, onReady }) => {
-  const videoRef = React.useRef<HTMLVideoElement | null>(null);
-  const playerRef = React.useRef<VideoJsPlayer | null>(null);
+describe('VideoJS', () => {
 
-  React.useEffect(() => {
-    // Make sure Video.js player is only initialized once
-    if (!playerRef.current) {
-      const videoElement = videoRef.current;
+    it('resizes video correctly', () => {
 
-      // eslint-disable-next-line no-multi-assign
-      const player: VideoJsPlayer = (playerRef.current = videojs(videoElement, options, () => {
-        console.debug('player is ready');
-        return onReady && onReady(player, videojs);
-      }));
+        const { getByTestId } = render(<VideoJS />);
 
-      player.autoplay(options.autoplay);
-      player.src(options.sources);
-    }
+        const video = getByTestId('video');
 
-    // Add a cachebuster param to playlist URLs.
-    if (
-      (videojs.getPlayer(videoRef.current).tech({ IWillNotUseThisInPlugins: true }) as any)?.vhs
-    ) {
-      (
-        videojs.getPlayer(videoRef.current).tech({ IWillNotUseThisInPlugins: true }) as any
-      ).vhs.xhr.beforeRequest = o => {
-        if (o.uri.match('m3u8')) {
-          const cachebuster = Math.random().toString(16).substr(2, 8);
-          // eslint-disable-next-line no-param-reassign
-          o.uri = `${o.uri}?cachebust=${cachebuster}`;
-        }
 
-        return o;
-      };
-    }
-  }, [options, videoRef]);
 
-  return (
-    <div data-vjs-player>
-      {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
-      <video
-        ref={videoRef}
-        className={`video-js vjs-big-play-centered vjs-show-big-play-button-on-pause ${styles.player} vjs-owncast`}
-      />
-    </div>
-  );
-};
+        expect(video).toHaveStyle('width: 50%');
+
+
+
+        fireEvent.resize(video, { target: { innerWidth: 1024 } });
+
+
+
+        expect(video).toHaveStyle('width: 100%');
+
+    });
+
+});
