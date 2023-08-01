5b84a8931ebb079b0c0576660cfc813396620812
Richard Abrich <richard.abrich@gmail.com>
2023-08-01 09:02:51 -0400

    QAI commit for issue 1690894841

--- web/components/video/VideoJS/VideoJS.tsx
+++ web/components/video/VideoJS/VideoJS.tsx
@@ -1,60 +1,46 @@
-import React, { FC } from 'react';
-import videojs from 'video.js';
-import type VideoJsPlayer from 'video.js/dist/types/player';
+const URL = '/api/ping';
+const INTERVAL = 4000;
 
-import styles from './VideoJS.module.scss';
+function ping() {
+  try {
+    fetch(URL);
+  } catch (e) {
+    console.error(e);
+  }
+}
 
-require('video.js/dist/video-js.css');
+class ViewerPing {
+  timer: ReturnType<typeof setInterval>;
 
-export type VideoJSProps = {
-  options: any;
-  onReady: (player: VideoJsPlayer, vjsInstance: typeof videojs) => void;
-};
+  start() {
+    this.stop();
 
-export const VideoJS: FC<VideoJSProps> = ({ options, onReady }) => {
-  const videoRef = React.useRef<HTMLVideoElement | null>(null);
-  const playerRef = React.useRef<VideoJsPlayer | null>(null);
+    this.timer = setInterval(() => {
+      ping();
+    }, INTERVAL);
+  }
 
-  React.useEffect(() => {
-    // Make sure Video.js player is only initialized once
-    if (!playerRef.current) {
+  stop() {
+    clearInterval(this.timer);
+  }
+}
+
+export default ViewerPing;
+
+    // Resize video
+
+    const resizeVideo = () => {
+
       const videoElement = videoRef.current;
 
-      // eslint-disable-next-line no-multi-assign
-      const player: VideoJsPlayer = (playerRef.current = videojs(videoElement, options, () => {
-        console.debug('player is ready');
-        return onReady && onReady(player, videojs);
-      }));
+      videoElement.style.width = '100%';
 
-      player.autoplay(options.autoplay);
-      player.src(options.sources);
-    }
+      videoElement.style.height = '100%';
 
-    // Add a cachebuster param to playlist URLs.
-    if (
-      (videojs.getPlayer(videoRef.current).tech({ IWillNotUseThisInPlugins: true }) as any)?.vhs
-    ) {
-      (
-        videojs.getPlayer(videoRef.current).tech({ IWillNotUseThisInPlugins: true }) as any
-      ).vhs.xhr.beforeRequest = o => {
-        if (o.uri.match('m3u8')) {
-          const cachebuster = Math.random().toString(16).substr(2, 8);
-          // eslint-disable-next-line no-param-reassign
-          o.uri = `${o.uri}?cachebust=${cachebuster}`;
-        }
+    };
 
-        return o;
-      };
-    }
-  }, [options, videoRef]);
+    window.addEventListener('resize', resizeVideo);
 
-  return (
-    <div data-vjs-player>
-      {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
-      <video
-        ref={videoRef}
-        className={`video-js vjs-big-play-centered vjs-show-big-play-button-on-pause ${styles.player} vjs-owncast`}
-      />
-    </div>
-  );
-};
+    resizeVideo();
+
+
